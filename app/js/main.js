5cea833bd0b209ca494f1c9094c635e0b770a7c5
Apple <apple@MacBook-Pro.local>
2023-01-05 13:16:35 +0300

    added category

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
+  $('.category-top__item').on('click', function(e){
+    e.preventDefault();
+    $('.category-top__item').removeClass('category-top__item--active');
+    $(this).addClass('category-top__item--active');
+    
+    $('.category-content__item').removeClass('category-content__item--active');
+    $($(this).attr('href')).addClass('category-content__item--active');
+  });
+
   $('.header-inner__btn-profile').on('click', function () {
     $('.header-inner__btn-profileMenu').toggleClass('header-inner__btn-profileMenu--active');
   });
